8d4c1d94005fac6efb85674f00f44c0573e909b2
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-11-18 21:31:29 +0200

    [L4][HW] Add Page Object and tests for Login form

---
+++ pages/LoginForm.ts
@@ -0,0 +1,45 @@
+import { Locator, Page } from "@playwright/test";
+
+export class LoginForm {
+    readonly page : Page;
+    readonly loginForm : Locator;
+    readonly loginTitle : Locator;
+    readonly emailInput : Locator;
+    readonly passwordInput : Locator;
+    readonly loginButton : Locator;
+    readonly switchToRegistrationButton : Locator;
+
+    constructor(page : Page) {
+        this.page = page;
+        this.loginForm = page.locator('data-testid=login-form');
+        this.loginTitle = page.locator('data-testid=login-title');
+        this.emailInput = page.locator('data-testid=login-email-input');
+        this.passwordInput = page.locator('data-testid=login-password-input');
+        this.loginButton = page.locator('data-testid=login-submit-button');
+        this.switchToRegistrationButton = page.locator('data-testid=switch-to-register-button');
+    }
+    async goto() {
+        await this.page.goto('/');
+    }
+
+    async fillEmail(email : string) {
+        await this.emailInput.fill(email);
+    }
+
+    async fillPassword(password : string) {
+        await this.passwordInput.fill(password);
+    }
+    
+    async clickLoginButton() {
+        await this.loginButton.click();
+    }
+
+    async clickSwitchToRegistrationButton() {
+        await this.switchToRegistrationButton.click();
+    }
+
+    async fillLoginForm(email : string, password : string) {
+        await this.fillEmail(email);
+        await this.fillPassword(password);
+    }
+}
