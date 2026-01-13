34b680d2bd65b45be188067c33a88af1a0685902
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2026-01-13 17:51:48 +0100

    Fix azure pipelne

--- playwright.service.config.ts
+++ playwright.service.config.ts
@@ -4,6 +4,8 @@ import { DefaultAzureCredential } from '@azure/identity';
 import config from './playwright.config';
 
 /* Learn more about service configuration at https://aka.ms/pww/docs/config */
+// PLAYWRIGHT_SERVICE_URL should be set as an environment variable
+// It will be automatically picked up by createAzurePlaywrightConfig
 export default defineConfig(
   config,
   createAzurePlaywrightConfig(config, {
